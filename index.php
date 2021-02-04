1f281abf80b9a4c102748bce87685eb3b9e4aa5d
rjc0015 <rjc0015@mix.wvu.edu>
2021-02-04 10:53:04 -0500

    Lab 2 about page

---
+++ index.php
@@ -0,0 +1,55 @@
+<body>
+
+    <link rel="stylesheet" href="css/about.css">
+    
+
+        <div class = "about">
+            <hl>About US</hl>
+            <p>We are a band of wayward vagabonds that specialize in sensory overload. Do not be discouraged. Its only hearing loss</p>
+        </div>
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                    <img src="images/1.jpg">
+                    <div class="container">
+                        <h2>Chief Officer</h2>
+                        <p class="c1">Johnny Cash</p>
+                        <p>Email:ringoffire@cs230.com</p>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <img src="images/2.jpg">
+                    <div class="container">
+                        <h2>Janitorial Manager</h2>
+                        <p>Hank Williams</p>
+                        <p>Email:notthejr@cs230.com</p>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <img src="images/3.jpg">
+                    <div class="container">
+                        <h2>Co-Chief Officer</h2>
+                        <p>Bonny Clyde</p>
+                        <p>Email:bankrobberyrules@cs230.com</p>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <img src="images/4.jpg">
+                    <div class="container">
+                        <h2>Cafeteria Specialist</h2>
+                        <p>Ramsey Gordon</p>
+                        <p>Email:itseffingraw@cs230.com</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+</body>
